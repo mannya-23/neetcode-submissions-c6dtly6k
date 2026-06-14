ec96851b2c6c178a2bd0db6b144c4f3c9aa99181
mannya-23 <142636525+mannya-23@users.noreply.github.com>
2026-06-14 10:08:23 -0700

    Add: duplicate-integer - submission-9

---
+++ Data Structures & Algorithms/duplicate-integer/submission-9.py
@@ -0,0 +1,8 @@
+from collections import Counter
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        count = Counter(nums)
+        for num in count:
+            if count[num] > 1:
+                return True 
+        return False
